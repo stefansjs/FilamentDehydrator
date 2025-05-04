f66777a4af45035a2547bffc59b9feb85974bd57
Stefan Sullivan <stefan.sullivan@gmail.com>
2025-05-03 18:25:14 -0700

    Create a basic main that chooses which app to run based on config.toml

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-import asyncio
-from drybox.hardware import Heater
-from microapp.microapp import MicroApp
+from external import tomli as toml
 
 def main():
-    heater = Heater(pin=1)
-    # hygrometer = Hygrometer()
+    config = read_config()
+    main = config.get("main", "test")
+    if main == "test":
+        print("Running test")
+        from test import main as test_main
+        test_main()
+
+    elif main == "drybox":
+        print("Running drybox")
+        from drybox.hardware import Heater
+        from microapp.microapp import MicroApp
+
+        heater = Heater(pin=1)
+        # hygrometer = Hygrometer()
+        app = MicroApp()
+        app()
+
+    else:
+        print("no known main; running test")
+        from test import main as test_main
+        test_main()
+
+def read_config(path="config.toml"):
+    try:
+        with open(path, "rb") as f:
+            return toml.load(f)
+    except OSError as e:
+        print("Config file error: ", e)
+        return {}
 
 if __name__ == "__main__":
-    app = MicroApp()
-    asyncio.run(app.run(main))
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("terminated by user")
+    except BaseException as e:
+        import sys
+        sys.print_exception(e)
