66ef8a5c2cedad79982cf66369a8160308ba41aa
Stefan Sullivan <stefan.sullivan@gmail.com>
2025-05-03 17:40:28 -0700

    basic tests for temperature

--- test.py
+++ test.py
@@ -3,25 +3,32 @@ from utime import sleep, sleep_ms
 
 
 LED_PIN = "LED"
-TEMP_PIN = "TEMP"
-pin = Pin("LED", Pin.OUT)
+TEMP_PIN = 4
+led = Pin("LED", Pin.OUT)
 
 
 def main():
 
-    pin.off()
+    led.off()
     print("Starting up")
     # Flash once for startup
     flash(pause_after=1000)
 
     # Check temperature pin
-    temp_pin = ADC(Pin(TEMP_PIN, Pin.IN))
+    temp_pin = ADC(TEMP_PIN)
     print(f"Temperature pin: {temp_pin.read_u16()}")
+    if temp_pin.read_u16() in (0, 65535):
+        raise ValueError("Temperature pin value is suspicious")
+    temp_voltage = temp_pin.read_u16() * 3.3 / 65535
+    print(f"Temperature voltage: {temp_voltage}V")
+    temp_celsius = 27 - (temp_voltage - 0.706) / 0.001721
+    print(f"Temperature: {temp_celsius}º°C")
     # Flash twice for temperature check
     flash(times=2, pause_after=1000)
 
-    # End by turning on the LED permanently
-    pin.on()
+    # End by flashing the LED indefinitely
+    while True:
+        flash(duration_ms=250)
 
 
 def flash(duration_ms=100, times=1, pause_before=None, pause_after=0):
@@ -38,9 +45,9 @@ def flash(duration_ms=100, times=1, pause_before=None, pause_after=0):
     sleep_ms(pause_before)
     for _ in range(times):
         sleep_ms(duration_ms)
-        pin.on()
+        led.on()
         sleep_ms(duration_ms)
-        pin.off()
+        led.off()
     sleep_ms(pause_after)
 
 
@@ -51,8 +58,7 @@ if __name__ == '__main__':
     
     except Exception as e:
         print(f"An error occurred: {e}")
-        while True:
-            flash(duration_ms=250)
+        led.on()
     
     else:
         print("Done.")
