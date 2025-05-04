cda563e1621dfdb8e43d79a97cdddc25040d782a
Stefan Sullivan <stefan.sullivan@gmail.com>
2025-05-04 00:14:37 -0700

    I refactored the Analog pin class to take scale and offset rather than trying to calculate it Added main to hardware.py

--- drybox/hardware.py
+++ drybox/hardware.py
@@ -3,19 +3,18 @@ from pico.pin import Analog  # Import Pin and ADC from the machine module
 
 
 class Thermister:
-    """
-    A class to represent a thermister sensor.
-    """
-
-    def __init__(self, pin, min_temp: float = 0, max_temp: float = 100):
-        self.pin = Analog(Pin(pin, Pin.IN), min_temp, max_temp)
+    def __init__(self, pin, min_temp: float = 0, max_temp: float = 100, sensor_scaling: float = 1):
+        self.pin = Analog(pin, scale=sensor_scaling, offset=min_temp if sensor_scaling > 0 else max_temp)
+        self.min_temp = min_temp
+        self.max_temp = max_temp
+        self.sensor_scaling = sensor_scaling
         
 
     def get_temperature(self) -> float:
         """
         Returns temperature in Celsius.
         """
-        return self.pin.read_float()
+        return self.pin.read_float() 
     
     def get_temperature_fahrenheit(self) -> float:
         """
@@ -27,14 +26,14 @@ class Thermister:
 class Heater:
     UNSAFE_TEMPERATURE = 70
     UNSAFE_PICO_TEMPERATURE = 85  # From Pico datasheet
-    PICO_THERMISTER = Thermister(4, Pin.IN)
+    
 
     @staticmethod
     def check(*heaters):
         """
         Check if the heater is in a safe state. If the temperature exceeds the unsafe limit, panic.
         """
-        if Heater.PICO_THERMISTER.get_temperature() > Heater.UNSAFE_PICO_TEMPERATURE:
+        if Pico.PICO_THERMISTER.get_temperature() > Heater.UNSAFE_PICO_TEMPERATURE:
             print("Panic! Unsafe temperature detected.")
             return False
         
@@ -64,3 +63,48 @@ class Heater:
         self.pin.on()
         self.is_on = True
         print("Heater is ON")
+
+
+class Pico:
+    """
+    A class to hold singletons for the Pico board.
+
+
+    RPi Pico has a strange algorithm for the thermister.
+    The formula is:
+    V = 3.3 * ADC / 65535 = 3.3 * float_value
+    T = 27 - (V - 0.706) / 0.001721
+    I want to convert this to a much more straightforward linear equation.
+    So, if 
+       T = 27 - (V - 0.706) / 0.001721
+         = 27 - V / 0.001721 + 0.706 / 0.001721
+         = -V / 0.001721 + 27 + 0.706 / 0.001721
+         = -(3.3 * float_value) / 0.001721 + 27 + 0.706 / 0.001721
+         = float_value * -3.3 / 0.001721 + 27 + 0.706 / 0.001721
+    """
+
+    PICO_SCALE = -3.3 / 0.001721  # = -1917.4898...
+    PICO_OFFSET = 27 + 0.706 / 0.001721  # = 437.22661
+    PICO_MIN_TEMP = PICO_SCALE + PICO_OFFSET  # 27 - (3.3V - 0.706) / 0.001721 = 27 - 2.594 / 0.001721 = -1480.26
+    PICO_MAX_TEMP = PICO_OFFSET  # 27 - (0 - 0.706) / 0.001721 = 27 + 0.706 / 0.001721 = 437.227
+
+    PICO_THERMISTER = Thermister(4, min_temp=PICO_OFFSET, max_temp=PICO_MAX_TEMP, sensor_scaling=PICO_SCALE)
+
+
+if __name__ == "__main__":
+    from collections import OrderedDict
+    from utime import sleep_ms
+
+    sensors = OrderedDict([
+        ('Pico temperature', Pico.PICO_THERMISTER.get_temperature,),
+    ])
+
+    print("; ".join(sensors.keys()))
+
+    while True:
+        try:
+            values = [str(getter()) for getter in sensors.values()]
+            print("; ".join(values))
+            sleep_ms(500)
+        except KeyboardInterrupt:
+            break
--- pico/pin.py
+++ pico/pin.py
@@ -1,17 +1,27 @@
-from machine import ADC, PinLike
+from machine import ADC
 
 class Analog:
     """
     a wrapper for ADC that converts to float
     """
-
     TO_FLOAT = 1/65535
 
-    def __init__(self, pin: PinLike, min_output: float = 0, max_output: float = 1):
+    @staticmethod
+    def build(pin, min_output, max_output):
+        """ Maps a [0 1] range to a [min_output max_output] range """
+        scale = (max_output - min_output)
+        offset = min_output
+        return Analog(pin, scale, offset)
+
+    def __init__(self, pin, scale:float = 1, offset:float=0):
         self.adc = ADC(pin)
-        self.scale = (max_output - min_output) * self.TO_FLOAT
-        self.offset = min_output 
+        self.scale = scale * self.TO_FLOAT 
+        self.offset = offset
 
     def read_float(self):
         # read an analog value as an int and convert to float
         return self.adc.read_u16() * self.scale + self.offset
+    
+    def read_int(self):
+        # read an analog value as an int
+        return self.adc.read_u16()
