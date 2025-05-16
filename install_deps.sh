22b85473a4aa911e654215253ce8c1fc45ee7c6e
Stefan Sullivan <stefan.sullivan@gmail.com>
2025-05-16 01:01:10 -0700

    pin newer version of DhtReader

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mpremote mip install github:stefansjs/rp2_dht_reader@v2.0
+mpremote mip install github:stefansjs/rp2_dht_reader@v2.1
 mpremote mip install github:stefansjs/micropython-tomli@v2.0-lite
